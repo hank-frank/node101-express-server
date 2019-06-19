1a486576bdf9d9721014419bf5ea9886456f3974
hankfrank <henryfrank1@yahoo.com>
2019-06-19 10:58:13 -0700

    commit of initial files

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 // import files and packages up here
-
-
+const express = require('express'); 
+const morgan = require('morgan');
+const data = require('./data.json')
 // create your express server below
-var app;
+var app = express();
 
+app.use(morgan('dev'));
+
+app.get('/', function(req, res){
+    res.status(200).send('New Express Server')
+})
 // add your routes and middleware below
+app.get('/data', function(req, res){
+    res.status(200);
+    res.send(data);
+})
 
+app.get('*', function(req, res){
+    res.status(200,send('No Page Found'));
+});
 // finally export the express application
 module.exports = app;
